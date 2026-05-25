863d5b6b645b349f865a408c8590ee0edac1093d
Manuel Hernandez <manu.hdz.dev@gmail.com>
2026-05-24 18:00:24 -0600

    chore: init spring boot project with jpa and postgres

---
+++ src/main/java/com/example/apiemergencias/ApiEmergenciasApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiemergencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEmergenciasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiEmergenciasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apiemergencias/ApiEmergenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiemergencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEmergenciasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
